545e1002aab1dee1d286091f6b9372c2b85498d8
Koro234 <tra.ne555222@mail.ru>
2021-07-05 21:50:01 +0400

    Start

---
+++ HtmlParserTest/Core/HTMLParser/HTMLParserSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlParserTest.Core.HTMLParser
+{
+    class HTMLParserSettings : IParserSettings
+    {
+        public string BaseUrl { get; set; } = "https://habr.com/ru/";
+        public string Prefix { get; set; } = "page{CurrentId}";
+        public int StartPoint { get; set; }
+        public int EndPoint { get; set; }
+    }
+}
---
+++ HtmlParserTest/Core/IParserSettings.cs
@@ -0,0 +1,11 @@
+﻿
+namespace HtmlParserTest.Core
+{
+    interface IParserSettings
+    {
+        string BaseUrl { get; set; }
+        string Prefix { get; set; }
+        int StartPoint { get; set; }
+        int EndPoint { get; set; }
+    }
+}
